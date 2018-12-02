821e9b3527c67a210e9b12fe2d191c88dc1b1428
小工匠 <yangshangwei@users.noreply.github.com>
2018-12-02 21:44:59 +0800

    将微服务注册到Eureka Server上

--- micorservice-provider-user/src/main/java/com/artisan/microservice/MicorserviceSimpleProviderUserApplication.java
+++ micorservice-provider-user/src/main/java/com/artisan/microservice/MicorserviceSimpleProviderUserApplication.java
@@ -2,8 +2,10 @@ package com.artisan.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication 
+@EnableDiscoveryClient
 public class MicorserviceSimpleProviderUserApplication {
 
 	public static void main(String[] args) {
