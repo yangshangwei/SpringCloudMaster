f5741933d4b860c3b10e8589ad220d634ce54944
yangshangwei <yangshangwei@inspur.com>
2018-12-09 18:23:53 +0800

    micorservice-consumer-movie-ribbon-customrule-propertie工程

---
+++ micorservice-consumer-movie-ribbon-customrule-properties/src/main/java/com/artisan/micorservice/model/User.java
@@ -0,0 +1,18 @@
+package com.artisan.micorservice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 8912111288470833198L;
+	private Long id;
+	private String username;
+	private String name;
+	private Integer age;
+	private BigDecimal balance;
+
+}
