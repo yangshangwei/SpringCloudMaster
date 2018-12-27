4ed537ac79e4b4afa325309eae532d77a1ab33d4
yangshangwei <yangshangwei@inspur.com>
2018-12-27 09:20:47 +0800

    Feign项目整合Hystrix监控

--- micorservice-consumer-movie-fegin-hystrix/src/main/java/com/artisan/micorservice/MicorserviceConsumerFeginHystrixApplication.java
+++ micorservice-consumer-movie-fegin-hystrix/src/main/java/com/artisan/micorservice/MicorserviceConsumerFeginHystrixApplication.java
@@ -2,12 +2,14 @@ package com.artisan.micorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class MicorserviceConsumerFeginHystrixApplication {
 	
 	public static void main(String[] args) {
